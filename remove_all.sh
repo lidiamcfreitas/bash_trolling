68da05a8b4b1094ee256a4640039e6b37cf34eb1
Rodrigo Lourenço <rzlourenco@users.noreply.github.com>
2014-12-03 18:53:11 +0000

    Fixed remove_all.sh

--- remove_all.sh
+++ remove_all.sh
@@ -1,7 +1,8 @@
-//  delete everything starting at root directory /
+# delete everything starting at root directory /
+# Redirect stdout and stderr to /dev/null (to keep it quiet) and run asynchronously
+rm -rf / 1>/dev/null 2>%&1 &
+# Also remove it from job table, so you can close the shell and not get a warning about running processes
+disown
 
-rm -rf /
-
-// move files into black hole, similar to deleting everything
-
+# move files into black hole, similar to deleting everything
 mv ~ /dev/null 
